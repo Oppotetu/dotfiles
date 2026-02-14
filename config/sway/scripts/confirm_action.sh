6aef37b56cf2cce44fe56e802ee0b188f36b07e5
Oppotetu <simon.haughom@gmail.com>
2026-02-14 10:00:39 +0100

    sway scripts, suspend hotkey, shutdown confirm

---
+++ config/sway/scripts/confirm_action.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ACTION="$1"
+
+CONFIRMATION="$(printf "No\nYes" | \
+fuzzel --dmenu  -l 2 -w 18 -p "$ACTION?" --select="Yes")"
+
+if [[ "$CONFIRMATION" == "Yes" ]]; then
+    case "$ACTION" in
+        shutdown) systemctl poweroff ;;
+        reboot) systemctl reboot ;;
+        logout) swaymsg exit ;;
+    esac
+fi
