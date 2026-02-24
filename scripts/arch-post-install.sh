9cf72c67d812dc74962241de4afc96d0403e0b85
Oppotetu <simon.haughom@gmail.com>
2026-02-24 20:32:15 +0100

    cleanup script

--- scripts/arch-post-install.sh
+++ scripts/arch-post-install.sh
@@ -1,7 +1,17 @@
-
+#!/bin/bash
 
 #######################################
 # Cleanup
 #######################################
 
-sudo pacman -Rns greetd-regreet greetd gtklock gtklock-userinfo-module gtklock-powerbar-module
+sudo pacman -Rns \
+  greetd-regreet greetd gtklock \
+  gtklock-userinfo-module gtklock-powerbar-module \
+  azote foot nwg-drawer
+
+
+sudo rm -iv "$HOME"/.config/reflector-simple-free-params.txt
+sudo rm -iv "$HOME"/.config/welcome_news_latest_date_file
+sudo rm -iv "$HOME"/.azotebg
+sudo rm -riv "$HOME"/.config/foot
+sudo rm -riv "$HOME"/.config/nwg-drawer
