62ebe4e585ff93f0c1fdda1c2a6c7df84af3f4d1
Simon Haughom <sohaugho@uio.no>
2026-03-01 09:39:05 +0100

    mac defaults

--- config/mac-defaults.sh
+++ config/mac-defaults.sh
@@ -4,17 +4,21 @@ defaults write com.google.Chrome NSUserKeyEquivalents -dict \
     'New Tab' '^t' \
     'New Tab to the Right' '^g' \
     'Close Tab' '^w' \
-    'Select Next Tab' '^m' \
-    'Select Previous Tab' '^n' \
+    'Select Next Tab' '$^k' \
+    'Select Previous Tab' '$^j' \
     'Reopen Closed Tab' '^b' \
-    'Duplicate Tab' '^u' \
     'Select All' '^a' \
     'Print…' '^p' \
     'Find…' '^f' \
     'Bookmark This Tab…' '^d' \
     'Open Location…' '^l' \
+    'Back' '$^h' \
+    'Forward' '$^l'
+
+killall "Google Chrome"
+sleep 3
+open -a "Google Chrome"
 
 
-# Restart Chrome for change to take effect
-# killall "Google Chrome" 2>/dev/null || true
-# open -a "Google Chrome"
+    # 'Duplicate Tab' '^u' \
+
