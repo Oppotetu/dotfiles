6c983dbbbbdf9458dde02ce2b767a45e63159efa
Simon Haughom <simon.haughom@gmail.com>
2026-03-14 15:19:57 +0100

    matugen wip, hypridle

--- config/localbin/wallpaper-picker.sh
+++ config/localbin/wallpaper-picker.sh
@@ -1,48 +1,20 @@
 #!/usr/bin/sh
 
-# dependencies: hyprpaper, fzf, a terminal with sixel support
 
-# this is a script to set a wallpaper for hyprpaper on a single monitor setup
-# ipc must be turned on inside hyprpaper.conf
-
-# bgdir="$HOME/Pictures/nature" # the directory where all wallpapers are stored
-
-# # check if sixel is installed, if not -exit
-# if ! command -v img2sixel &>/dev/null; then
-#   echo "img2sixel not found."
-#   exit 1
-# fi
-
-# # use fzf with sixel previews to select wallpaper
-# bgfile=$(ls "$bgdir" | fzf --height 100% \
-#   --preview "img2sixel --width 1200 --height auto -q low  $bgdir/{} 2>/dev/null" \
-#   --preview-window=right:75%:wrap)
-
-# #  check if wallpaper was selected, if not - exit.
-# if [ -z "$bgfile" ]; then
-#   echo "No wallpaper selected..."
-#   exit 1
-# fi
-
-# # apply selected wallpaper
-# wallust.sh "$bgdir"/"$bgfile"
-
-
-
-
-#!/bin/bash
 # Pick wallpaper
 wallpaper=$(find ~/Pictures/wallpapers -type f \( -iname "*.jpg" -o -iname "*.png" \) | fuzzel --dmenu)
 
 # Pick mode
-mode=$(echo -e "dark\nlight" | fuzzel --dmenu --prompt "Theme: ")
+# mode=$(echo -e "dark\nlight" | fuzzel --dmenu --prompt "Theme: ")
 
-case "$mode" in
-  dark)   wallust run "$wallpaper" --palette ansidark16 --skip-sequences ;;
-  light)  wallust run "$wallpaper" --palette ansilight16 ;;
-esac
+# case "$mode" in
+#   dark)   wallust run "$wallpaper" --palette ansidark16 --skip-sequences ;;
+#   light)  wallust run "$wallpaper" --palette ansilight16 ;;
+# esac
+
+matugen image "$wallpaper" --verbose --show-colors
 
 hyprctl hyprpaper wallpaper ", $wallpaper"
 
-# Auto-reload apps
-pkill -USR2 waybar
+# # Auto-reload apps
+# pkill -USR2 waybar
