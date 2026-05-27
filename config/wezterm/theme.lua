e5400be74b9e2e178acb0c20ac036a9df77c396c
Simon Haughom <sohaugho@uio.no>
2026-05-27 16:09:32 +0200

    vsc, nvim

--- config/wezterm/theme.lua
+++ config/wezterm/theme.lua
@@ -2,7 +2,8 @@ local wezterm = require("wezterm")
 local colors = require("wezterm-colors")
 return function(config)
     -- config.color_scheme = "Catppuccin Mocha"
+    config.color_scheme = 'Lost Woods (terminal.sexy)'
     -- config.color_scheme = 'Solarized Light (Gogh)'
-    config.colors = colors.colors
-    config.window_frame = colors.window_frame
+    -- config.colors = colors.colors
+    -- config.window_frame = colors.window_frame
 end
